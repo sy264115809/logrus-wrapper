f0944d9a14bce97af9a16ba81444b9987d7d4dfb
sy264115809 <264115809@qq.com>
2017-01-10 20:06:17 +0800

    add wrapper of entry

--- logger.go
+++ logger.go
@@ -14,6 +14,29 @@ type Logger struct {
 	prefix string
 }
 
+// Entry proxies the logrus.Entry to remove namespace dependency
+type Entry struct {
+	*logrus.Entry
+}
+
+// WithField is the proxy of entry.WithField
+func (e *Entry) WithField(key string, value interface{}) *Entry {
+	e.Entry = e.Entry.WithField(key, value)
+	return e
+}
+
+// WithFields is the proxy of entry.WithFields
+func (e *Entry) WithFields(fields Fields) *Entry {
+	e.Entry = e.Entry.WithFields(logrus.Fields(fields))
+	return e
+}
+
+// WithError is the proxy of entry.WithError
+func (e *Entry) WithError(err error) *Entry {
+	e.Entry = e.Entry.WithError(err)
+	return e
+}
+
 // Fields holds the data should be output in log.
 type Fields logrus.Fields
 
@@ -63,153 +86,155 @@ func (logger *Logger) Copy(prefix ...string) *Logger {
 	}
 }
 
-// NewEntry returns a wrapper of logrus.Entry
-func (logger *Logger) entry() *logrus.Entry {
+// NewEntry returns an entry Entry
+func (logger *Logger) entry() *Entry {
 	entry := logrus.NewEntry(logger.Logger)
 	if logger.prefix != "" {
 		entry = entry.WithField("prefix", logger.prefix)
 	}
-	return entry
+	return &Entry{
+		Entry: entry,
+	}
 }
 
 // WithField adds a field to the log entry, note that you it doesn't log until you call
 // Debug, Print, Info, Warn, Fatal or Panic. It only creates a log entry.
 // If you want multiple fields, use `WithFields`.
-func (logger *Logger) WithField(key string, value interface{}) *logrus.Entry {
+func (logger *Logger) WithField(key string, value interface{}) *Entry {
 	return logger.entry().WithField(key, value)
 }
 
 // WithFields adds a struct of fields to the log entry. All it does is call `WithField` for
 // each `Field`.
-func (logger *Logger) WithFields(fields Fields) *logrus.Entry {
-	return logger.entry().WithFields(logrus.Fields(fields))
+func (logger *Logger) WithFields(fields Fields) *Entry {
+	return logger.entry().WithFields(fields)
 }
 
 // WithError adds an error as single field to the log entry.  All it does is call
 // `WithError` for the given `error`.
-func (logger *Logger) WithError(err error) *logrus.Entry {
+func (logger *Logger) WithError(err error) *Entry {
 	return logger.entry().WithError(err)
 }
 
-// Debugf is the proxy of entry.Debugf()
+// Debugf is the proxy of logger.Debugf()
 func (logger *Logger) Debugf(format string, args ...interface{}) {
 	logger.entry().Debugf(format, args...)
 }
 
-// Infof is the proxy of entry.Infof()
+// Infof is the proxy of logger.Infof()
 func (logger *Logger) Infof(format string, args ...interface{}) {
 	logger.entry().Infof(format, args...)
 }
 
-// Printf is the proxy of entry.Printf()
+// Printf is the proxy of logger.Printf()
 func (logger *Logger) Printf(format string, args ...interface{}) {
 	logger.entry().Printf(format, args...)
 }
 
-// Warnf is the proxy of entry.Warnf()
+// Warnf is the proxy of logger.Warnf()
 func (logger *Logger) Warnf(format string, args ...interface{}) {
 	logger.entry().Warnf(format, args...)
 }
 
-// Warningf is the proxy of entry.Warningf()
+// Warningf is the proxy of logger.Warningf()
 func (logger *Logger) Warningf(format string, args ...interface{}) {
 	logger.entry().Warnf(format, args...)
 }
 
-// Errorf is the proxy of entry.Errorf()
+// Errorf is the proxy of logger.Errorf()
 func (logger *Logger) Errorf(format string, args ...interface{}) {
 	logger.entry().Errorf(format, args...)
 }
 
-// Fatalf is the proxy of entry.Fatalf()
+// Fatalf is the proxy of logger.Fatalf()
 func (logger *Logger) Fatalf(format string, args ...interface{}) {
 	logger.entry().Fatalf(format, args...)
 	os.Exit(1)
 }
 
-// Panicf is the proxy of entry.Panicf()
+// Panicf is the proxy of logger.Panicf()
 func (logger *Logger) Panicf(format string, args ...interface{}) {
 	logger.entry().Panicf(format, args...)
 }
 
-// Debug is the proxy of entry.Debug()
+// Debug is the proxy of logger.Debug()
 func (logger *Logger) Debug(args ...interface{}) {
 	logger.entry().Debug(args...)
 }
 
-// Info is the proxy of entry.Info()
+// Info is the proxy of logger.Info()
 func (logger *Logger) Info(args ...interface{}) {
 	logger.entry().Info(args...)
 }
 
-// Print is the proxy of entry.Print()
+// Print is the proxy of logger.Print()
 func (logger *Logger) Print(args ...interface{}) {
 	logger.entry().Info(args...)
 }
 
-// Warn is the proxy of entry.Warn()
+// Warn is the proxy of logger.Warn()
 func (logger *Logger) Warn(args ...interface{}) {
 	logger.entry().Warn(args...)
 }
 
-// Warning is the proxy of entry.Warning()
+// Warning is the proxy of logger.Warning()
 func (logger *Logger) Warning(args ...interface{}) {
 	logger.entry().Warn(args...)
 }
 
-// Error is the proxy of entry.Error()
+// Error is the proxy of logger.Error()
 func (logger *Logger) Error(args ...interface{}) {
 	logger.entry().Error(args...)
 }
 
-// Fatal is the proxy of entry.Fatal()
+// Fatal is the proxy of logger.Fatal()
 func (logger *Logger) Fatal(args ...interface{}) {
 	logger.entry().Fatal(args...)
 	os.Exit(1)
 }
 
-// Panic is the proxy of entry.Panic()
+// Panic is the proxy of logger.Panic()
 func (logger *Logger) Panic(args ...interface{}) {
 	logger.entry().Panic(args...)
 }
 
-// Debugln is the proxy of entry.Debugln()
+// Debugln is the proxy of logger.Debugln()
 func (logger *Logger) Debugln(args ...interface{}) {
 	logger.entry().Debugln(args...)
 }
 
-// Infoln is the proxy of entry.Infoln()
+// Infoln is the proxy of logger.Infoln()
 func (logger *Logger) Infoln(args ...interface{}) {
 	logger.entry().Infoln(args...)
 }
 
-// Println is the proxy of entry.Println()
+// Println is the proxy of logger.Println()
 func (logger *Logger) Println(args ...interface{}) {
 	logger.entry().Println(args...)
 }
 
-// Warnln is the proxy of entry.Warnln()
+// Warnln is the proxy of logger.Warnln()
 func (logger *Logger) Warnln(args ...interface{}) {
 	logger.entry().Warnln(args...)
 }
 
-// Warningln is the proxy of entry.Warningln()
+// Warningln is the proxy of logger.Warningln()
 func (logger *Logger) Warningln(args ...interface{}) {
 	logger.entry().Warnln(args...)
 }
 
-// Errorln is the proxy of entry.Errorln()
+// Errorln is the proxy of logger.Errorln()
 func (logger *Logger) Errorln(args ...interface{}) {
 	logger.entry().Errorln(args...)
 }
 
-// Fatalln is the proxy of entry.Fatalln()
+// Fatalln is the proxy of logger.Fatalln()
 func (logger *Logger) Fatalln(args ...interface{}) {
 	logger.entry().Fatalln(args...)
 	os.Exit(1)
 }
 
-// Panicln is the proxy of entry.Panicln()
+// Panicln is the proxy of logger.Panicln()
 func (logger *Logger) Panicln(args ...interface{}) {
 	logger.entry().Panicln(args...)
 }
